5d4e9cd922ee46917c0d44eda26b09f34e2a04a5
gliggy <gliggy@users.noreply.github.com>
2018-02-17 11:16:14 -0500

    Update 15_game.js

--- 15_game.js
+++ 15_game.js
@@ -55,7 +55,8 @@ Vector.prototype.times = function(factor) {
 var actorChars = {
   "@": Player,
   "o": Coin,
-  "=": Lava, "|": Lava, "v": Lava
+  "=": Lava, "|": Lava, "v": Lava, "a": Lava
+  "H": Hint,
 };
 
 function Player(pos) {
